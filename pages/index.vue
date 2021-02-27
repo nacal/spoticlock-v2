fc4862aadc2fbf8f018dec95eaa09402af18132b
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-28 04:10:56 +0900

    Change bg color

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div
+    class="grid place-items-center min-h-screen bg-gradient-to-t from-green-900 to-primary p-5"
+  >
+    <MusicPlayerCard />
+  </div>
 </template>
 
 <script>
