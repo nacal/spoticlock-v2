64b1e2f3dfe7cac68a5cb4a148d30607984d73f8
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-28 07:36:43 +0900

    Develop default data

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,34 @@
     class="grid place-items-center min-h-screen bg-gradient-to-t from-green-900 to-primary p-5"
   >
     <Button />
-    <MusicPlayerCard />
+    <MusicPlayerCard :now-playing="nowPlaying" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      nowPlaying: {
+        item: {
+          artists: [
+            {
+              name: 'artist',
+            },
+          ],
+          name: 'music title',
+          album: {
+            images: [
+              {},
+              {
+                url:
+                  'https://upload.wikimedia.org/wikipedia/en/f/f1/Tycho_-_Epoch.jpg',
+              },
+            ],
+          },
+        },
+      },
+    }
+  },
+}
 </script>
