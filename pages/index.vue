3902093c4b318c1520b924456929719ae344501a
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-28 09:17:43 +0900

    Develop layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div
-    class="grid place-items-center min-h-screen bg-gradient-to-t from-green-900 to-primary p-5"
+    class="grid justify-center content-between min-h-screen w-full bg-gradient-to-t from-green-900 to-primary p-2"
   >
-    <Button />
-    <MusicPlayerCard :now-playing="nowPlaying" />
+    <header>
+      <h1 class="font-bold text-5xl text-center text-gray-900 mb-2 mt-8">
+        SpotiClock
+      </h1>
+      <p class="text-xs md:text-base text-center text-gray-900 mb-4">
+        A clock that shows the nowplaying song on spotify.
+      </p>
+      <Button />
+    </header>
+    <main>
+      <MusicPlayerCard :now-playing="nowPlaying" />
+    </main>
+    <footer class="text-center">
+      <small lang="en">&copy; 2021 hikaru nakata</small>
+    </footer>
   </div>
 </template>
 
