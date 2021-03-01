e006d217e9d6d8430cfe79d84e4baf3fb3ffa315
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-01 20:32:47 +0900

    Add pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,8 @@ export default {
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+
+    '@nuxtjs/pwa',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
